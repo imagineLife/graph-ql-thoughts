984e476cf9e636c55b62f6d28233912c1c9aaacf
Eric Laursen <ericjlaursen@gmail.com>
2020-11-14 07:02:35 -0500

    re-introduce manual filter here

--- petql/api/src/resolvers.js
+++ petql/api/src/resolvers.js
@@ -60,8 +60,8 @@ module.exports = {
     return [{id:1, name: 'init'}, {id: 2, name: 'hank'}] 
     },
     pet(_,{input},ctx){
-      // return myPets.filter(p => p.name === input.name)[0]
-      return ctx.models.Pet.findOne(input)
+      return myPets.filter(p => p.name === input.name)[0]
+      // return ctx.models.Pet.findOne(input)
     },
     petsByType(_,{input:{petType}},ctx){
       return myPets.filter(p => p.type === petType)
